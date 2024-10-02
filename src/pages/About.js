32f2d4455fb4bb7dc898747900f2574381e38d5f
jaime32 <Jamers4590@gmail.com>
2024-10-01 23:41:51 -0700

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './About.css';
+
+const About = () => {
+    return (
+        <div className="about">
+            <h1>Welcome to About page</h1>
+            <p>This is my about page</p>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Contact.css';
+
+const Contact = () => {
+    return (
+        <div className="contact">
+            <h1>Welcome to Contact page</h1>
+            <p>This is my contact page</p>
+        </div>
+    );
+};
+
+export default Contact;
