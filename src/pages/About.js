cf91b13b61bf10ae8125b7d60a12298795b32f45
Christopher Angel <christopher.angel17@gmail.com>
2024-12-07 16:26:04 -0800

    Home Changes
    Changed layout and format of Home page. Need to add some information to the last section.
    Also need to have other tabs match the layout of Home.

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,26 +3,32 @@ import './About.css';
 
 const About = () => {
     return (
+
         <div className="about">
+            <div class="container">
+            <link rel="stylesheet" type="text/css" href="About.css"></link>
             <h1>Welcome to Bonsai </h1>
-            <p>Where innovation meets challenge! Here, we break the mold of traditional climbing with our new style competition 
+            <div> 
+                <p class="intro-paragraph">Where innovation meets challenge! Here, we break the mold of traditional climbing with our new style competition 
                climbing that’s designed to be as fun for beginners as it is exhilarating for seasoned climbers.
-            </p>
+                </p>
+            </div>    
                 
                     <div className="about">
-                        <h1>What We Offer:</h1>
+                        <h2>What We Offer:</h2>
                         <ul>
-                            <li><b>Competition Climbing:</b> Our walls are tailored to offer a variety of dynamic routes that keep everyone engaged and coming back for more. </li>
-                            <li><b>Gym Facilities:</b> In addition to climbing, we offer a fully equipped small gym to complement your fitness regime.</li>
-                            <li><b>Study & Work Area:</b> Balancing school or work? Our quiet, comfortable study area is perfect for remote workers and students looking to hit the books or the keyboard between climbs.</li>
+                            <li class="restof-paragraph"><b>Competition Climbing:</b> Our walls are tailored to offer a variety of dynamic routes that keep everyone engaged and coming back for more. </li>
+                            <li class="restof-paragraph"><b>Gym Facilities:</b> In addition to climbing, we offer a fully equipped small gym to complement your fitness regime.</li>
+                            <li class="restof-paragraph"><b>Study & Work Area:</b> Balancing school or work? Our quiet, comfortable study area is perfect for remote workers and students looking to hit the books or the keyboard between climbs.</li>
                         </ul>
 
-                        <h1> Ready to Conquer?</h1>
-                        <p> Join our community at Bonsai Climbing Gym—where every climb is a new adventure, and every member is part of the family. 💪🌳</p>
-                        <h2>At Bonsai, climbing isn't just a sport, it’s an experience. </h2>
+                        <h2> Ready to Conquer?</h2>
+                        <p class="restof-paragraph"> Join our community at Bonsai Climbing Gym—where every climb is a new adventure, and every member is part of the family. 💪🌳</p>
+                        <p class="bottom-paragraph">At Bonsai, climbing isn't just a sport, it’s an experience. </p>
               
 
                     </div>
+                </div>
         
         
         
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,26 +5,62 @@ const Home = () => {
     return (
         <div>
             <div className="hero-section">
-                <div className="overlay">
-                    <div className="hero-content">
-                        <h1>The climbing gym where any beginner becomes an expert</h1>
-                        <p>Come in and have fun</p>
-                        <button className="gym-btn">Join the Gym!</button>
+                <div className=".hero-section2">
+                <div className="hero-content">
+                            <h1>Bonsai</h1>
+                            <h2>100s of Boulder Problems <br></br> 100,00sq ft Climbing Terrain</h2>
+                            <p>Join our community! 
+                            <button className="gym-btn">Sign Up </button> </p>
+                        </div>
+                    <div className="overlay">
                     </div>
                 </div>
             </div>
             <div className="info-section">
-                <h2>Welcome to our gym</h2>
-                <p>Here we can add more information</p>
+                    <h2>Where climbers train to reach new heights. </h2>
+                    <h3>    ⭐⭐⭐⭐⭐ <br></br><br></br> ​"This place is awesome! I have <br></br>thoroughly enjoyed every last <br></br>
+                            bit of this bouldering gym. <br></br> The staff is always so <br></br>nice & eager to help you. The <br></br>
+                            facilities are also top notch <br></br> & always clean as well!"<br></br
+                            >​- Aurora B. <br></br><br></br><br></br><br></br>
+
+                            ⭐⭐⭐⭐⭐ <br></br><br></br> "I spend enough time at Bonsai<br></br> that I could consider it a <br></br>second home. The staff are<br></br> 
+                            always friendly and stoked, the <br></br>routes are always awesome, and <br></br>the facilities are always clean." <br></br>
+                            ​- Nicholas A. <br></br><br></br><br></br><br></br>
+
+                            ⭐⭐⭐⭐⭐ <br></br><br></br>​"Amazingly friendly staff, helped me<br></br> with any questions I had about<br></br> the gym and were overall friendly.<br></br> 
+                            Helped me try on shoes and <br></br> talked to me about what to get<br></br> and what to look for. I highly <br></br>recommend this gym for climbing!"<br></br>
+                            - Gavin F.
+                    
+
+                    </h3>
+
+                    <h4>
+                        ⭐⭐⭐⭐⭐ <br></br><br></br>"Bonsai is awesome! Great<br></br> gym, amazing staff, and a <br></br>welcoming climbing community! <br></br>Flowstones climbing, gym, <br></br>
+                        and yoga classes made <br></br> canceling my old membership<br></br>  an obvious choice!"<br></br>
+                        ​- Shane G.<br></br><br></br><br></br><br></br>
+
+                        ⭐⭐⭐⭐⭐ <br></br><br></br>​"Bonsai Climbing Gym is a <br></br>great gym that offers so much<br></br> for different abilities and age.  <br></br>They have have beginner classes,<br></br>
+                        climbing classes, hang bouldering <br></br> and indoor bouldering. They <br></br> have a yoga studio and classes."<br></br>
+                        ​- Debbie S.<br></br><br></br><br></br>
+
+                        ⭐⭐⭐⭐⭐ <br></br><br></br> "​A clean and modern climbing <br></br> gym. The workout equipment <br></br> was new and in great shape. <br></br> I loved that they open <br></br> the garage doors on one <br></br> 
+                        wall to let natural lighting <br></br> and air in. Friendly and <br></br> calm energy." <br></br>
+                        ​- Vanessa M.
+
+
+
+                        
+                        
+                        
+                    </h4>   
+                        
+                        <p></p>
             </div>
             <div className="reachout-section">
                 <h2>This is to reach out</h2>
-                <p>here we can add more information</p>
+                <p></p>
             </div>
-            <div className="end-section">
-                <h2>This will be the end of the page</h2>
-                <p>Here we can add more information</p>
-            </div>
+
         </div>
     );
 };
